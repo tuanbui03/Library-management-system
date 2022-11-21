3a2f022767ebcc30dbbe3433d9d434e73ebf4f7a
Huwnga <hungbungbu2003@gmail.com>
2022-11-21 18:10:32 +0700

    add view book, categories, authors, publishing, admin_nav

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,8 @@
-/* doesn't work with source level 1.8:
-module com.mycompany.mavenproject1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.mavenproject1 to javafx.fxml;
-    exports com.mycompany.mavenproject1;
-}
-*/
+//module com.mycompany.mavenproject1 {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//    requires java.base;
+//
+//    opens com.mycompany.mavenproject1 to javafx.fxml;
+//    exports com.mycompany.mavenproject1;
+//}
